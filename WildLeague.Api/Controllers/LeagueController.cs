02007c42e50c1828c2bf96b6b5099b9c267556a5
bjuzwicki <bartlomiej.juzwicki@wp.pl>
2025-01-01 21:45:48 +0100

    init solution (clean architecture,ddd)

---
+++ WildLeague.Api/Controllers/LeagueController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WildLeague.API.Controllers
+{
+	public class LeagueController : Controller
+	{
+		
+	}
+}
---
+++ WildLeague.Api/Controllers/LiveMatchController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WildLeague.Api.Controllers
+{
+	public class LiveMatchController : Controller
+	{
+	}
+}
